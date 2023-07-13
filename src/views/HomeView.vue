ec0acd7349b1e637ad596040373049c07865f8ad
jeff239 <bgnon044@gmail.com>
2023-07-13 11:15:54 +0000

    first

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="container mt-5">
+      <ul class="list-group">
+        <li class="list-group-item" v-for="(data, index) in news" v-bind:key="index">
+          Date :{{ data.date_et_heure }} <br>
+          Type: {{  data.type }}<br>
+          Titre {{  data.titre }}<br>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import axios from 'axios';
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+type News = {
+  type: string;
+  titre: string;
+  date_et_heure: string;
+}
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
+  data(){
+    return {
+      news:[] as News[],
+      length:1
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      axios.get('http://localhost:3000/news/'+this.length)
+      .then((response) => {
+        response.data.length > 0 && this.news.push(response.data)
+       this.length++    
+      })
+}, 1000);
+
+      
   },
 });
 </script>
